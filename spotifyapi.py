11b363ec1bf17b1991ce479b242b89933a963928
zylstraa <zylstra.az@gmail.com>
2021-05-29 11:06:26 -0500

    working on iterating through curl

--- spotifyapi.py
+++ spotifyapi.py
@@ -9,8 +9,9 @@
 
 
 
+
 curl -X "GET" 
-    "https://api.spotify.com/v1/search?q=Justin%20Bieber&type=artist" 
+    "https://api.spotify.com/v1/search?q='H.E.R.%20Slide'&type=artist,track" 
     -H "Accept: application/json" 
     -H "Content-Type: application/json" 
-    -H "Authorization: Bearer BQA4GOu9mqZv9kOT-OMYYWdReT3ijjvvrj8F-2ALmZGZWXF8mfR8Fqurwss3xCqNmqVyuI70A5obpHgRXP95N83Myh2x9eK304oLQE1EcuqMshg3QnSdmN2q8Dkx6ACnkyAfiJf2zB6GEqxZk1XqC2NTWk8WDvjVfGfXhoHsrkoKdR9VqC32p24rtA1BU1HacC8"
+    -H "Authorization: Bearer BQC8x8ymN9MuhnJi55BcpYnBTSUFfgx3S0ppNrpYSBKhNGT2hUroOlcP13g8mHPxKmM66AnzRanGFumuIq0Hiv3gavP8FxPZfLGeZXGPb3a_CSupHeF1kINYgsBS8wYsvCODq-yelbGZ9irjO4aY0yp-s_tBR6OdyHmNGgbjQb47twMklvIg1CfbQyK72Lq19Gg"
